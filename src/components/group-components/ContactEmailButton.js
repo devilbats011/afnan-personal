7616d62f5f7cdfa1dc5a7f6c2e3e2e8c40f2f216
Afnan <wanafnanharizwz@gmail.com>
2020-12-16 17:55:37 +0800

    Add input,button & its logic to contactEmailButton.js

--- src/components/group-components/ContactEmailButton.js
+++ src/components/group-components/ContactEmailButton.js
@@ -1,39 +1,66 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from "react";
 
-function ContactEmailButton({firebase}) {
+function ContactEmailButton({ firebase }) {
+  const inputEmailRef = useRef(null)
+  const [message, setMessage] = useState("no response yet..")
+  const [emailRespond,setEmailRespond] = useState("NO-RES-YET")
+  const [isDisable,setIsDisable] = useState(false)
 
-    const [message, setMessage] = useState("no response yet..")
-
-    async function clickHandler(e){
-        if(firebase === false){
-            setMessage("firebase--sdk--disable")
-            return null 
-        } 
-        const result = await firebase
-        .firestore()
-        .collection("theEmail")
-        .add({
-          email: "contactemail@btn.com",
-        })
-        .then(function (docRef) {
-          console.log("Document written with ID: ", docRef.id);
-          return "Document written with ID: " + docRef.id;
-        })
-        .catch(function (error) {
-          console.error("Error adding document: ", error);
-          return "Error adding document";
-        });
-        setMessage(result)
+  async function clickHandler(e) {
+    setIsDisable(true)
+    console.log('inputEl.current.value:', )
+    if (firebase === false) {
+      setMessage("firebase--sdk--disable")
+      return null;
     }
+    const emailData = inputEmailRef.current.value
+    if(emailData === '' || emailData === null){
+        setMessage("Field Cannot be empty.")
+        setIsDisable(false)
+        return null
+    }
+    const result = await firebase
+      .firestore()
+      .collection("theEmail")
+      .add({
+        email: emailData,
+      })
+      .then(function (docRef) {
+        setIsDisable(false)
+        setEmailRespond("EMAIL-SAVED")
+        console.log("Document written with ID: ", docRef.id ,emailData);
+        return "Document written with ID: " + docRef.id + `, Email saved : ${emailData} `;
+    })
+    .catch(function (error) {
+        setIsDisable(false)
+        setEmailRespond("EMAIL-UNSAVED")
+        console.error("Error adding document: ", error);
+        return "Error adding document";
+      });
+    setMessage(result);
+  }
 
-    return (
-        <div>
-                <button className="contact__btn" onClick={(e) => clickHandler(e)} disabled={false} >
-                    contactemail@btn.com
-                </button>
-                <p>message: {message} </p>
-        </div>
-    )
+  return (
+    <div>
+      <section className={emailRespond === "EMAIL-SAVED" ? "relative hidden" : "relative"}>
+        <input
+          ref={inputEmailRef}
+          type="text"
+          name="email"
+          className="contact__emailinput"
+          placeholder="Drop an email, let me know"
+        />
+        <button
+          className="contact__btn"
+          onClick={(e) => clickHandler(e)}
+          disabled={isDisable}
+        >
+          email
+        </button>
+      </section>
+        <p>message: {message} </p>
+    </div>
+  );
 }
 
-export default ContactEmailButton
+export default ContactEmailButton;
